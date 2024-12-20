d15db716e5864410a7498457fa33edd09fce3566
Gabcutiepie <fen_hart@yahoo.com>
2024-12-20 10:43:54 +0000

    DAY 4

--- Day4Folder/day4.py
+++ Day4Folder/day4.py
@@ -8,14 +8,14 @@
 #    counter1=counter1+1
 
 #counter2=5
-# while counter2 <33:
-#     print(counter2)
-#     counter2=counter2+1
+#while counter2 <33:
+#    print(counter2)
+#    counter2=counter2+1
 
-# counter3=50
-# while counter3 >0:
-#     print(counter3)
-#     counter3=counter3+ -1
+#counter3=50
+#while counter3 >0:
+#    print(counter3)
+#    counter3=counter3+ -1
 
 riddleAns="meh"
 userAns = input("What sound does a sheep make? ")
@@ -30,6 +30,27 @@ while userAns != riddleAns:
     userAns=input("What sound does a sheep make? ")
     counter=counter+1
 print("You are correct! You took " + str(counter) + " attempts.")
+
+riddleAns="SEE O DOUBLE YOU"
+userAns = input("How do you spell COW in thirteen letters? ")
+counter=1
+while userAns != riddleAns:
+    if counter ==5:
+        print("Hint: All the words are capitalized.")
+    if counter ==10:
+        print("Hint: There are 4 words.")
+    if counter ==15:
+        print("Hint: The answer starts with SEE _ ______ ___.")
+    if counter ==20:
+        print("Hint: The answer starts with SEE O ______ ___.")
+    if counter ==25:
+        print("Hint: The answer starts with SEE O DOUBLE ___.")
+    if counter ==30:
+        print("The answer is SEE O DOUBLE YOU.")
+    userAns=input("How do you spell COW in thirteen letters? ")
+    counter=counter+1
+print("You are correct! You took " + str(counter) + " attempts.")
+
 
 
 
